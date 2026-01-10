50cc1cf913d257e0e99be13a17ebc49c17dd13e1
Azaken1248 <suvkrm@gmail.com>
2026-01-10 15:39:40 +0530

    add cors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os, shutil, uuid, zipfile, json, asyncio, httpx, re
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Request
 from fastapi.responses import FileResponse, Response
+from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
@@ -9,6 +10,14 @@ from osrparse import Replay
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI(title="osu! Render Manager v4.4")
 app.state.limiter = limiter
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 @app.exception_handler(RateLimitExceeded)
 async def custom_rate_limit_handler(request: Request, exc: RateLimitExceeded) -> Response:
